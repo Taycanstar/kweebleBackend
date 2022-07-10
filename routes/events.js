e71918d9f83524d5138a0e4ae501a6df604ccc56
Taycanstar <diminunez06@gmail.com>
2022-07-09 19:44:39 -0500

    f

--- routes/events.js
+++ routes/events.js
@@ -27,6 +27,8 @@ const upload = multer({ storage: storage });
 
 router.use("/image", express.static("uploads"));
 
+router.use("/icon", express.static("uploads"));
+
 router.post(
   "/image",
   requireLogin,
@@ -53,6 +55,35 @@ router.post(
     }
 
     res.status(200).json({ imageURL: req.file.path });
+  }
+);
+
+router.post(
+  "/icon",
+  requireLogin,
+  upload.single("icon"),
+  async (req, res) => {
+    console.log("File is: ", req.file);
+
+    if (req.file.size > 2 * 3000 * 3000) {
+      res.status(400).json({ error: "max file size of 2MB exceeded" });
+      return;
+    }
+
+    let ext;
+    switch (req.file.mimetype) {
+      case "image/jpeg":
+        ext = "jpg";
+        break;
+      case "image/png":
+        ext = "png";
+        break;
+      default:
+        res.status(400).json({ error: "bad content type" });
+        return;
+    }
+
+    res.status(200).json({ iconURL: req.file.path });
   }
 );
 
