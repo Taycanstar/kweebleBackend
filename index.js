01223debf53d3698b44c4743497a31e3c4c1abad
Taycanstar <diminunez06@gmail.com>
2022-07-15 15:58:41 -0500

    ew

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ app.use("/api", require("./routes/data"));
 app.use("/events", require("./routes/events"));
 app.use("/messages", require("./routes/messages"));
 
-
-
 const pusher = new Pusher({
   appId: "1437686",
   key: "1a01f4a6cdee8f5ea5a3",
@@ -29,24 +27,37 @@ pusher.trigger("my-channel", "my-event", {
   message: "hello world",
 });
 
-
-
 //db config
 const CONNECTION_URL =
   "mongodb+srv://dimerson:7l3QhcdYXc4H8fNG@cluster0.76ok5.mongodb.net/KweebleDatabase?retryWrites=true&w=majority";
-  mongoose.connect(CONNECTION_URL, {
+mongoose.connect(CONNECTION_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
 
-const db = mongoose.connection 
+const db = mongoose.connection;
 
 db.once("open", () => {
-  console.log("db connected")
+  console.log("db connected");
 
-  const msgCollection = db.collection("Message")
-})
+  const msgCollection = db.collection("messages");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log(" A change occured", change);
+
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.user,
+        text: messageDetails.message,
+      });
+    } else {
+      console.log("Error triggering pusher");
+    }
+  });
+});
 
 mongoose.set("useFindAndModify", false);
 
