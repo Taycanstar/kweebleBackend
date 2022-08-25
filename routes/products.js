184f80ed98ee6580d7d0f0cfc66d23e2b3f75963
Taycanstar <diminunez06@gmail.com>
2022-08-24 19:07:03 -0500

    hi

--- routes/products.js
+++ routes/products.js
@@ -44,7 +44,7 @@ const multi_upload = multer({
   },
 }).array("uploadedImages", 10);
 
-router.post("/", (req, res) => {
+router.post("/image", (req, res) => {
   multi_upload(req, res, function (err) {
     if (err instanceof multer.MulterError) {
       // A Multer error occurred when uploading.
@@ -77,35 +77,6 @@ router.post("/", (req, res) => {
     res.status(200).end("Your files uploaded.");
   });
 });
-
-router.post(
-  "/image",
-  requireLogin,
-  upload.array("image", 10),
-  async (req, res) => {
-    console.log("File is: ", req.file);
-
-    if (req.file.size > 2 * 3000 * 3000) {
-      res.status(400).json({ error: "max file size of 2MB exceeded" });
-      return;
-    }
-
-    let ext;
-    switch (req.file.mimetype) {
-      case "image/jpeg":
-        ext = "jpg";
-        break;
-      case "image/png":
-        ext = "png";
-        break;
-      default:
-        res.status(400).json({ error: "bad content type" });
-        return;
-    }
-
-    res.status(200).json({ imageURL: req.file.path });
-  }
-);
 
 //Add product
 router.post("/", async (req, res) => {
