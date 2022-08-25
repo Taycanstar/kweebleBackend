6c666c87ae9a79bbe8f9a221d86b9c7cc7457a50
Taycanstar <diminunez06@gmail.com>
2022-08-24 21:09:29 -0500

    hi

--- routes/products.js
+++ routes/products.js
@@ -25,58 +25,30 @@ const storage = new CloudinaryStorage({
   },
 });
 
-const multi_upload = multer({
-  storage,
-  limits: { fileSize: 25 * 3000 * 3000 }, // 1MB
-  fileFilter: (req, file, cb) => {
-    if (
-      file.mimetype == "image/png" ||
-      file.mimetype == "image/jpg" ||
-      file.mimetype == "image/jpeg"
-    ) {
-      cb(null, true);
-    } else {
-      cb(null, false);
-      const err = new Error("Only .png, .jpg and .jpeg format allowed!");
-      err.name = "ExtensionError";
-      return cb(err);
-    }
-  },
-}).array("uploadedImages", 10);
+const upload = multer({ storage: storage });
 
-router.post("/image", (req, res) => {
-  multi_upload(req, res, function (err) {
-    if (err instanceof multer.MulterError) {
-      // A Multer error occurred when uploading.
-      res
-        .status(500)
-        .send({ error: { message: `Multer uploading error: ${err.message}` } })
-        .end();
-      return;
-    } else if (err) {
-      // An unknown error occurred when uploading.
-      if (err.name == "ExtensionError") {
-        res
-          .status(413)
-          .send({ error: { message: err.message } })
-          .end();
-      } else {
-        res
-          .status(500)
-          .send({
-            error: { message: `unknown uploading error: ${err.message}` },
-          })
-          .end();
-      }
-      return;
-    }
+router.post("/image", upload.any(), async (req, res) => {
+  console.log("File is: ", req.file);
 
-    // Everything went fine.
-    // show file `req.files`
-    // show body `req.body`
-    // res.status(200).end("Your files uploaded.");
-    res.status(200).json({ imagesURL: req.file.path });
-  });
+  if (req.file.size > 2 * 3000 * 3000) {
+    res.status(400).json({ error: "max file size of 2MB exceeded" });
+    return;
+  }
+
+  let ext;
+  switch (req.file.mimetype) {
+    case "image/jpeg":
+      ext = "jpg";
+      break;
+    case "image/png":
+      ext = "png";
+      break;
+    default:
+      res.status(400).json({ error: "bad content type" });
+      return;
+  }
+
+  res.status(200).json({ imageURL: req.file.path });
 });
 
 //Add product
