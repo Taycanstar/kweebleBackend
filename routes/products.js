3a9a2caf176918ff4d645d58bdb790bbae3c12bd
Taycanstar <diminunez06@gmail.com>
2022-08-27 00:33:35 -0500

    hi

--- routes/products.js
+++ routes/products.js
@@ -26,35 +26,61 @@ const storage = new CloudinaryStorage({
 const upload = multer({ storage: storage });
 router.use("/image", express.static("uploads"));
 
-router.post(
-  "/image",
+// router.post(
+//   "/image",
 
-  upload.array("image", 3),
-  async (req, res, next) => {
-    //   if (size > 25 * 1024 * 1024) {
-    //     res.status(400).json({ error: "max file size of 2MB exceeded" });
-    //     return;
-    //   }
+//   upload.array("image", 3),
+//   async (req, res, next) => {
+//     //   if (size > 25 * 1024 * 1024) {
+//     //     res.status(400).json({ error: "max file size of 2MB exceeded" });
+//     //     return;
+//     //   }
 
-    //   let ext;
-    //   switch (type) {
-    //     case "image/jpeg":
-    //       ext = "jpg";
-    //       break;
-    //     case "image/png":
-    //       ext = "png";
-    //       break;
-    //     default:
-    //       res.status(400).json({ error: "bad content type" });
-    //       return;
-    //   }
-    console.log("resp", res);
-    console.log("req", req);
-    console.log("next", next);
+//     //   let ext;
+//     //   switch (type) {
+//     //     case "image/jpeg":
+//     //       ext = "jpg";
+//     //       break;
+//     //     case "image/png":
+//     //       ext = "png";
+//     //       break;
+//     //     default:
+//     //       res.status(400).json({ error: "bad content type" });
+//     //       return;
+//     //   }
+//     console.log("resp", res);
+//     console.log("req", req);
+//     console.log("next", next);
 
-    return res.status(200).json({ content: req.body });
+//     return res.status(200).json({ content: req.body });
+//   }
+// );
+
+router.post("/image", upload.single("image"), async (req, res) => {
+  console.log("File is: ", req.file);
+
+  if (req.file.size > 2 * 3000 * 3000) {
+    res.status(400).json({ error: "max file size of 2MB exceeded" });
+    return;
   }
-);
+
+  let ext;
+  switch (req.file.mimetype) {
+    case "image/jpeg":
+      ext = "jpg";
+      break;
+    case "image/png":
+      ext = "png";
+      break;
+    default:
+      res.status(400).json({ error: "bad content type" });
+      return;
+  }
+
+  console.log("ress", res);
+
+  res.status(200).json({ imageURL: req.file.path });
+});
 
 const multi_upload = multer({
   storage,
