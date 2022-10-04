987eb401fcbbd83ef2f7e4bcf28c1d274a46b8f9
Taycanstar <diminunez06@gmail.com>
2022-10-04 16:03:54 -0400

    msg

---
+++ models/Scope.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const productSchema = require("./Product");
+const eventSchema = require("./Event");
+const messageSchema = require("./Message");
+
+const scopeSchema = new Schema({
+  name: {
+    type: String,
+  },
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  products: [productSchema],
+  events: [eventSchema],
+  messages: [messageSchema],
+});
+
+const Scope = mongoose.model("Scope", scopeSchema);
+
+module.exports = Scope;
