4b977c1896f6b711a76f0f742f8a220df19e7656
Taycanstar <diminunez06@gmail.com>
2023-06-13 21:24:16 -0500

    d

--- routes/events.js
+++ routes/events.js
@@ -175,8 +175,8 @@ router.post("/", async (req, res) => {
     console.log(docRef, "hey");
     if (notificationTime != null) {
       await docRef.set({
-        title: `${name} is starting at soon!`,
-        body: `${description}. It will start at ${startTime}`,
+        title: `${name} is starting soon!`,
+        body: `${description} --Starting at ${startTime}`,
         topic: scope,
         data: {},
         sent: false,
