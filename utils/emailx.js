055d5a30c41d8f4faa94df84abd3fced091ba3ff
Taycanstar <diminunez06@gmail.com>
2023-07-24 10:46:20 -0400

    ress

---
+++ utils/emailx.js
@@ -0,0 +1,19 @@
+// var postmark = require("postmark");
+
+// const api = process.env.POSTMARK_API_TOKEN;
+
+// const sendEmail = async (options) => {
+//   // Send an email:
+//   var client = new postmark.ServerClient(api);
+
+//   client.sendEmail({
+//     From: "noreply@kweeble.com",
+//     To: options.email,
+//     Subject: options.subject,
+//     HtmlBody: options.message,
+//     TextBody: options.message,
+//     MessageStream: "ResetPassword",
+//   });
+// };
+
+// module.exports = sendEmail;
